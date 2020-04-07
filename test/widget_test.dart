3a266860e1096a6931859094d5fd378c860eae1b
reducedcarpet <michaelcadenpike@gmail.com>
2020-04-07 16:53:23 +0100

    Tests should work now, added index.html for flutter web to maybe work

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,6 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:dice_poker/main.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -15,11 +14,11 @@ void main() {
     await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
-    //expect(find.text('0'), findsOneWidget);
-    //expect(find.text('1'), findsNothing);
+    expect(find.text('Roll'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.alarm));
-    await tester.pump();
+    //await tester.tap(find.byIcon(Icons.alarm));
+    //await tester.pump();
   });
 }
