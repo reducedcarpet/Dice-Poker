30cab98d285ee9191fcc92fb83ae1a67c45fa270
reducedcarpet <michaelcadenpike@gmail.com>
2020-04-10 14:32:02 +0100

    trying to fix chmod error

--- lib/models/game_turn.dart
+++ lib/models/game_turn.dart
@@ -6,7 +6,7 @@ final int servePoints = 10;
 class GameTurn extends Equatable {
   final List<Roll> rolls;
   //int scoreOne = 0;
-  //int scoreTwo = 0;
+  // int scoreTwo = 0;
 
   GameTurn(this.rolls);
 
